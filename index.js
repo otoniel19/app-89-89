8c57b68edba536e3892eef83e9fa3c46d268b131
otoniel19 <otonielcostaoliveira3@gmail.com>
2021-09-30 11:16:11 -0300

    all

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-  res.send("Ola mundo")
+  res.sendFile(__dirname + "/mess.html")
 })
 
 app.listen(process.env.PORT)
