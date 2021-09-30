8c33676cfbac5466176df1874497026528f533cc
otoniel19 <otonielcostaoliveira3@gmail.com>
2021-09-30 11:22:48 -0300

    /foda

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ app.get('/',(req,res) => {
   res.sendFile(__dirname + "/mess.html")
 })
 
+ap.get("/foda",(req,res) => {
+  res.json({mensagem: {
+    tipo: "Feliz",
+    conteudo: "Muito foda!"
+  }})
+})
+
 app.listen(process.env.PORT)
